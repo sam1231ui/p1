36f657fdc97f6f2d72b4973ee8e2628086250af8
sam1231ui <64959366+sam1231ui@users.noreply.github.com>
2020-07-16 19:21:52 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+n = int(input("enter a value for n:"))
+a=0
+b=1
+sum=0
+count=1
+print("fibonacci series: ",end =" ")
+while(count <= n):
+    print(sum,end=" ")
+    count +=1
+    a=b
+    b=sum
+    sum=a+b
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
