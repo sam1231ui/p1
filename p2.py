5250b9f8a2467054312ff141b91a5e0e10bf47ba
sam1231ui <64959366+sam1231ui@users.noreply.github.com>
2020-07-16 23:51:42 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[59]:
+
+
+list = [12,-7,5,64,-14]
+for val in list:
+    if val > 0:
+        print(val,end = " ")
+    
+        
+ 
+
+
+# In[60]:
+
+
+list = [12,14,-95,3]
+for val in list:
+    if val > 0:
+        print(val,end = " ")
+
+
+# In[ ]:
+
+
+
+
